b1b49d15586416d7cae77e2abb0b262dd229b382
blockdb <dup2snow@gmail.com>
2019-03-26 12:09:02 +0800

    Create discovery.go

---
+++ pkg/discovery/discovery.go
@@ -0,0 +1,17 @@
+package discovery
+
+import (
+    "time"
+)
+
+type Balancer interface {
+    Get() (ed *Endpoint, err error)
+    Vote(host string, port int, score int)
+}
+
+type Endpoint struct {
+    Host string
+    Port int
+    ReadTimeout time.Duration
+    WriteTimeout time.Duration
+}
