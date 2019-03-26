c4f2853d93272c59d476e2b880e956531b2c6712
blockdb <dup2snow@gmail.com>
2019-03-26 12:01:06 +0800

    Create logger.go

---
+++ pkg/logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+type Logger interface {
+    Trace(ctx context.Context, args ...interface{})
+    Tracef(ctx context.Context, format string, args ...interface{})
+    Debug(ctx context.Context, args ...interface{})
+    Debugf(ctx context.Context, format string, args ...interface{})
+    Info(ctx context.Context, args ...interface{})
+    Infof(ctx context.Context, format string, args ...interface{})
+    Warn(ctx context.Context, args ...interface{})
+    Warnf(ctx context.Context, format string, args ...interface{})
+    Error(ctx context.Context, args ...interface{})
+    Errorf(ctx context.Context, format string, args ...interface{})
+}
+    
